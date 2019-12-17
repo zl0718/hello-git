143839a95aa6b04850f642f4a100e154bbff9c82
zhangsan <zhang@163.com>
2019-12-17 15:48:35 +0800

    把工程添加到git

---
+++ src/com/zero/git/GitClass.java
@@ -0,0 +1,11 @@
+package com.zero.git;
+
+public class GitClass {
+    public static void main(String [] args){
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+    }
+}
